7b130687e1a001eb99349f03f35c4f448a8c39f9
Prakhar Singh Dhaila <prakhar.dhaila@comprotechnologies.com>
2020-09-24 07:40:54 +0530

    Updates

---
+++ precache-manifest.3030ed995bc386affbe4ab7261daaf68.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc59e204adc3002e50bd767f6600f6f8",
+    "url": "/ready-steady-bang/index.html"
+  },
+  {
+    "revision": "7e268830f32cca1d6a62",
+    "url": "/ready-steady-bang/static/js/2.b3fe98fc.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/ready-steady-bang/static/js/2.b3fe98fc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "65607fd9da5bdc73d0b4",
+    "url": "/ready-steady-bang/static/js/main.c1d709aa.chunk.js"
+  },
+  {
+    "revision": "b9430d266121f4ab2c3a",
+    "url": "/ready-steady-bang/static/js/runtime-main.e5253cff.js"
+  }
+]);
